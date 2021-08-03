0c508a6fe448addaa1c678bf369de8533705332d
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-03 19:16:53 +0000

    [fix]

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module com.github/Kinoshita0623/rpc-chat
 
 go 1.16
 
-require golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+require (
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.12 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"com.github/Kinoshita0623/rpc-chat/app/src/repository/impl"
 )
 
 func main() {
 	fmt.Printf("Hello go")
+	connectionInfo := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true", "test", "password", "db:3306", "database")
+
+	db, err := gorm.Open(mysql.Open(connectionInfo), &gorm.Config{})
+	if err != nil {
+		os.Exit(1)
+	}
+
+	rf := impl.Repository{
+		DB: db,
+	}
+	rf.UserRepository().Find(int64(1))
 }
