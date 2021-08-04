62353b9fcba1b698a0cd23cffff525e04271f12f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-04 13:29:07 +0000

    [fix]

--- app/src/repository/impl/post_repository_db.go
+++ app/src/repository/impl/post_repository_db.go
@@ -12,12 +12,15 @@ func (self *PostRepositoryDB) Create(post *entity.Post) (*entity.Post, error) {
 	if result := self.Repository.DB.Create(post); result.Error != nil {
 		return nil, result.Error
 	}
+	if result := self.Repository.DB.First(post, post.Id); result.Error != nil {
+		return nil, result.Error
+	}
 	return post, nil
 }
 
 func (self *PostRepositoryDB) FindAll(untilId *int64) ([]*entity.Post, error) {
 	var posts []*entity.Post
-	if result := self.Repository.DB.Where("id < ?", untilId).Order("id desc").Limit(100).Find(&posts); result.Error != nil {
+	if result := self.Repository.DB.Where("id < ?", untilId).Order("id desc").Limit(100).Preload(clause.Associations).Find(&posts); result.Error != nil {
 		return nil, result.Error
 	}
 	return posts, nil
